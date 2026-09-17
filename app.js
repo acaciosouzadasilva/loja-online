134d93262d719d0d8d35a9612ea2197d6b612c74
acaciosouzadasilva <acaciosouzadasilva@gmail.com>
2026-09-17 20:22:21 -0300

    fix: alterei a logica da conta do valor

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function calcularTotal(itens){
     let total = 0;
     for (let i = 0; i < itens.length; i++) {
-        total += itens[i].preco * 1.1;
+        total += itens[i].preco;
     }
      
     
